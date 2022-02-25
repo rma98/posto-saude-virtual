673ca5548092a2f6196b5004c363fdde78544a25
Hiany <hgfs@discente.ifpe.edu.br>
2022-02-25 09:51:34 -0300

    Começa codigo da tela principal nao logada

---
+++ src/components/PrincipalNaoLogada.vue
@@ -0,0 +1,56 @@
+<template>
+<div style="display:inline-block;vertical-align:top;">
+  <img class="logo" src="../assets/logoLogin.jpg">
+</div>
+<div style="display:inline-block;">
+  <h1 id="titulo">
+    Posto Saúde Virtual
+  </h1>
+</div>
+<div>
+  <nav class="navbar navbar-light">
+    <div class="container-fluid">
+        <a class="navbar-brand"> 
+          Medicos 
+        </a>
+    </div>
+  </nav>
+</div>  
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+.navbar{
+  background-color:#00008B;
+}
+.navbar-brand{
+  color:white;
+}
+
+body {
+    padding: 0;
+    margin: 0;
+    background-color: #454d6b;
+}
+
+.logo{
+  width:80%;
+  border-radius: 100px;
+  margin: 35%;
+  margin-left: 70%;
+}
+
+#titulo {
+  color: #00008B;
+  margin-left: 20%;
+  margin-top: 15%;
+   
+}
+</style>
