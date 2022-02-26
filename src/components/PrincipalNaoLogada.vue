4a72a5fe06b52824a5a6ed8a75377cd5be89723b
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2022-02-26 04:12:04 -0300

    Novas alterações no código HTML

--- src/components/PrincipalNaoLogada.vue
+++ src/components/PrincipalNaoLogada.vue
@@ -1,11 +1,29 @@
 <template>
-  <nav class="navbar navbar-light">
+  <nav class="navbar">
       <div class="container-fluid">
         <a class="navbar-brand">
           <img class="logo" src="../assets/logoLogin.jpg" >
-          Posto Saúde Virtual
+          <Strong>Posto Saúde Virtual</Strong>
         </a>
       </div>
+  </nav>
+
+  <nav class="navbar" id="menu">
+    <div class="container-fluid">
+      <nav class="nav">
+        <a class="nav-link active" aria-current="page" href="#"> <h4><strong>Médicos</strong></h4></a>
+        <a class="nav-link" href="#"> <h4><strong>Serviços</strong></h4></a>
+        <a class="nav-link" href="#"> <h4><strong>Postos</strong></h4></a>
+        <a class="nav-link" href="#"> 
+            <form action="#">
+              <input type="text" name="search" id="search" required>
+              <button type="submit"><i class="fas fa-search"></i></button>
+            </form>
+        </a>
+        <a class="nav-link" href="#"> <h4><strong>Login</strong></h4></a>
+        <a class="nav-link" href="#"> <h4><strong>Cadastrar</strong></h4></a>
+      </nav>
+    </div>
   </nav>
 </template>
 <script>
@@ -18,11 +36,50 @@ export default {
 </script>
 
 <style scoped>
-.navbar{
-  background-color:#00008B;
+.navbar {
+  background-color:#ffffff;
+  padding: 0.2em;
+  margin: 0.5em;
 }
+
+#menu {
+  background-color:#051775;
+}
+
+.logo{
+  width:19%;
+  border-radius: 100px;
+}
+
 .navbar-brand{
-  color:white;
+  color: #051775;
+}
+
+strong {
+  font-size: 20pt;
+  margin-left: 0.5em;
+}
+
+.nav-link {
+  color: #ffffff;
+}
+
+form {
+  border: 1px solid #ffffff;
+  border-radius: 0.3em;
+  padding: 0.3em;
+} 
+
+ form input, form button {
+  font-size: 1em;
+  border-radius: 0.2em;
+  border: none;
+  padding: 0.2em;
+}
+
+form button {
+  background: none;
+  color: #ffffff;
 }
 
 body {
@@ -31,17 +88,4 @@ body {
     background-color: #454d6b;
 }
 
-.logo{
-  width:80%;
-  border-radius: 100px;
-  margin: 35%;
-  margin-left: 70%;
-}
-
-#titulo {
-  color: #00008B;
-  margin-left: 20%;
-  margin-top: 15%;
-   
-}
 </style>
